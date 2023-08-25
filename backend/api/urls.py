a851121fafef5b2909b1361308b386a241e4eb98
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2023-08-25 13:48:38 +0800

    Feature: Updated routes_views.py view to display all available endpoints

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,11 +4,14 @@ from api.views.currency_views import *
 from api.views.routes_views import *
 
 urlpatterns = [
-    path('get-case-details', getCsCaseDetails),
+    path("", getViewRoutes),
+    path("view-case-routes", getCaseViews),
+    path("view-currency-routes", getCurrencyViews),
+
+    path('get-all-case-details', getCsCaseDetails),
     path('get-case-item-order-histogram', getCsCaseOrderHistory),
     path('get-case-item-price-history', getCsCasePriceHistory),
 
+    path("get-all-currency-exchange-rate", getCurrencyExchangeRates),
 
-
-    path("get-currency-exchange-rate", getCurrencyExchangeRates)
 ]
--- backend/api/views/routes_views.py
+++ backend/api/views/routes_views.py
@@ -8,25 +8,64 @@ import json
 
 from .privatekeys import *
 
+# API call to get the api endpoints relating to their views
 @api_view(['GET'])
 def getViewRoutes(request):
-    
+    routes = [
+            {
+            'Endpoint' : '/view-case-routes',
+            'method' : 'GET',
+            'body' : None,
+            'description' : 'Displays all the endpoints for getting "Counter Strike: Global Offensive" cases information.'
+        },
+            {
+            'Endpoint' : '/view-currency-routes',
+            'method' : 'GET',
+            'body' : None,
+            'description' : 'Displays all the endpoints for getting currency related information.'
+        },    
+    ]
 
 
-    return Response()
+    return Response(routes)
 
-
+# API call to get the api endpoints relating to currency related information
 @api_view(['GET'])
 def getCurrencyViews(request):
+    routes = [
+            {
+            'Endpoint' : '/get-all-currency-exchange-rate',
+            'method' : 'GET',
+            'body' : None,
+            'description' : 'Displays all currency exchange rate conversion information.'
+        },
+    ]
     
+    return Response(routes)
 
-
-    return Response()
-
-
+# API call to get the api endpoints relating to Counter Strike: Global Offensive case information
 @api_view(['GET'])
 def getCaseViews(request):
-    
+    routes = [
+            {
+            'Endpoint' : '/get-all-case-details',
+            'method' : 'GET',
+            'body' : None,
+            'description' : 'Displays all Counter Strike: Global Offensive case information'
+        },
+            {
+            'Endpoint' : '/get-case-item-order-histogram',
+            'method' : 'GET',
+            'body' : None,
+            'description' : 'Displays a Counter Strike: Global Offensive case order history.'
+        },    
+            {
+            'Endpoint' : '/get-case-item-price-history',
+            'method' : 'GET',
+            'body' : None,
+            'description' : 'Displays a Counter Strike: Global Offensive case price history.'
+        },    
+    ]
 
 
-    return Response()
+    return Response(routes)
